0a62ba9675e47d6736d2c69e603033aee352c470
Faisal <ayzukii@gmail.com>
2025-07-25 22:24:25 +0300

    Update set_permission.py

--- file-permission-project/set_permission.py
+++ file-permission-project/set_permission.py
@@ -2,5 +2,5 @@
 import os
 
 file_path = "example.py"
-os.chmod(file_path, 0o775) #rwxrwxr-x
-print(f"Permissions changed to rwxrwxr-x (775) for: {file_path}")
+os.chmod(file_path, 0o777) #rwxrwxr-x
+print(f"Permissions changed to rwxrwxr-x (777) for: {file_path}")
